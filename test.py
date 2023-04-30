af5baf337e94603bdceecf912126652f3283794c
Mohammad Kazemi Renany <43171147+MohamadKazemiRe9@users.noreply.github.com>
2023-04-30 17:33:42 +0430

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from my_key import my_api_key
+import openai
+
+openai.api_key = my_api_key
+# completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello"}])
+
+# print(completion.choices[0].message.content)
+
+
+
